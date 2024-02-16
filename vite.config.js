63efcc19747b58f6a9dc887d8c593aca83d59bd7
unknown <juaneguiabad1639@gmail.com>
2024-02-16 23:24:31 +0100

    modificado viteconfig y packagejson

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://juan1639.github.io/JuanEguiaAbad-portfolio-react'
 })
